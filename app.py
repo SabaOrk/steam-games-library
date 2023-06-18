6bf9ff6fcce110f69ca573c82f1e81ab7f2b3b04
Saba Orkoshneli <saba.orkoshneli16@gmail.com>
2023-06-18 18:51:38 +0400

    login signup

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import render_template, request, url_for, redirect
+from flask import render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,11 +11,34 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+app.secret_key = 'secret_key_8rh9238-h892brb2398rb29brc2'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 """
 MODELS
 """
+
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True,
+                         nullable=False)
+    password = db.Column(db.String(250),
+                         nullable=False)
+
+# Initialize app with extensions
+
+
+with app.app_context():
+    db.create_all()
+
+# Creates a user loader callback that returns the user object given an id
+
+
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
 
 
 class Game(db.Model):
@@ -50,3 +74,44 @@ def game(id):
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    # If the user made a POST request, create a new user
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        # Add the user to the database
+        db.session.add(user)
+        # Commit the changes made
+        db.session.commit()
+        # Once user account created, redirect them
+        # to login route (created later on)
+        return redirect(url_for("login"))
+    # Renders sign_up template if user made a GET request
+    return render_template("sign_up.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # If a post request was made, find the user by
+    # filtering for the username
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+        # Check if the password entered is the
+        # same as the user's password
+        if user.password == request.form.get("password"):
+            # Use the login_user method to log in the user
+            login_user(user)
+            return redirect(url_for("index"))
+        # Redirect the user back to the home
+        # (we'll create the home route in a moment)
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
